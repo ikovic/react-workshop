ae86fa0e90494193841389adc002f351bf33f59c
Izabela Domazet <izabela.domazet@gmail.com>
2017-10-05 21:45:36 +0200

    refactored input field

---
+++ src/components/inputField/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './inputField.css';
+
+const InputField = ({label, id, name }) => (
+  <div className="formRow">
+    <label htmlFor={id}>{label}</label>
+    <input
+      style={{ marginLeft: '4px' }}
+      type="text"
+      name={name}
+      id={id}
+      className="formInput"
+      ref={input => (this.tooltip = input)}
+    />
+  </div>
+);
+
+export default InputField;

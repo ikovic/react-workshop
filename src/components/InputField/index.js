3f09e7fff759e2ca32408fd26310526a8ed4bff6
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-10-19 20:39:38 +0200

    InputField api change, accepts value

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import './InputField.css';
 
-const InputField = ({ label, id, name, onChange }) => {
-  const wrappedOnChange = event => onChange(event.target.value);
-
-  return (
-    <div className="formRow">
-      <label htmlFor={id}>{label}</label>
-      <input
-        style={{ marginLeft: '4px' }}
-        type="text"
-        name={name}
-        id={id}
-        className="formInput"
-        onChange={wrappedOnChange}
-      />
-    </div>
-  );
-};
+const InputField = ({ label, id, name, onChange, value }) => (
+  <div className="formRow">
+    <label htmlFor={id}>{label}</label>
+    <input
+      style={{ marginLeft: '4px' }}
+      type="text"
+      name={name}
+      id={id}
+      className="formInput"
+      onChange={event => onChange(event.target.value)}
+      value={value}
+    />
+  </div>
+);
 
 export default InputField;
